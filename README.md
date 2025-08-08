ca30bf9d25965d1d3a5b4e24513fe42afd3f293c
sergii.ignatov <s.ignatov92@gmail.com>
2025-08-08 11:40:18 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Crypto dash micro project
+## part of react course
+
+using coingecko.com public API https://docs.coingecko.com/v3.0.1/reference/coins-markets
+
---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import {useState, useEffect} from 'react';
+import CoinCard from "./components/CoinCard.jsx";
+
+const API_URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+
+const App = () => {
+
+    const [coins, setCoins] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+
+        const fetchCoins = async () => {
+            try{
+               const response = await fetch(API_URL);
+               if (!response.ok) throw new Error('Could not fetch coins');
+               const data = await response.json();
+               setCoins(data);
+            } catch(err){
+                setError(err.message);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        fetchCoins();
+    }, [])
+
+    return (
+        <>
+            <h1>🚀 Crypto Dash</h1>
+            {loading && <p>Loading...</p>}
+            {error && <div className="error">{error}</div>}
+
+            {!error && !loading && (
+                <main className={"grid"}>
+                    {coins.map((coin) => (
+                        <CoinCard key={coin.id} coin={coin} />
+                    ))}
+                </main>
+            )}
+        </>
+    )
+}
+
+export default App
