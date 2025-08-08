1c430cc72a35e0802a301a4d106abad91c5faffb
sergii.ignatov <s.ignatov92@gmail.com>
2025-08-08 14:11:46 +0300

    Added display limit selector.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import {useState, useEffect} from 'react';
 import CoinCard from "./components/CoinCard.jsx";
+import LimitSelector from "./components/LimitSelector.jsx";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -8,12 +9,13 @@ const App = () => {
     const [coins, setCoins] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [limit, setLimit] = useState(10);
 
     useEffect(() => {
 
         const fetchCoins = async () => {
             try{
-               const response = await fetch(`${API_URL}?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false`);
+               const response = await fetch(`${API_URL}?vs_currency=usd&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`);
                if (!response.ok) throw new Error('Could not fetch coins');
                const data = await response.json();
                setCoins(data);
@@ -25,13 +27,15 @@ const App = () => {
         }
 
         fetchCoins();
-    }, [])
+    }, [limit])
 
     return (
         <>
             <h1>🚀 Crypto Dash</h1>
             {loading && <p>Loading...</p>}
             {error && <div className="error">{error}</div>}
+
+            <LimitSelector limit={limit} onLimitChange={setLimit} />
 
             {!error && !loading && (
                 <main className={"grid"}>
